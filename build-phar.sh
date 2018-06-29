367cc316a1f4ba069626c8cdf5bd9d53cf78c610
Michael Moravec <me@majkl.me>
2018-06-29 17:44:17 +0200

    Update BOX, tweak build script

--- build-phar.sh
+++ build-phar.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
-./download-box.sh
+if [ ! -f box.phar ]; then
+    wget https://github.com/humbug/box/releases/download/3.0.0-beta.4/box.phar -O box.phar
+fi
 
 # lock PHP to minimum allowed version
 composer config platform.php 7.1.0
-composer update
+composer update --no-dev
 
-php box.phar compile -vv
+php box.phar compile
 
-composer config --unset platform
+git checkout composer.*
+composer install
